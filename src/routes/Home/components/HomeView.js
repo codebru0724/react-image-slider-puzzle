3f5b5ead8c3ad6c14594c2fd640992ea14ad403b
LeiJun <leijun87@hotmail.com>
2017-05-05 09:28:32 +0800

    add game-button

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { IndexLink, Link } from 'react-router'
 import './HomeView.scss'
+
 
 export const HomeView = () => (
   <div>
     <h1>Enjoy My Image Slidding Puzzle Game</h1>
-    <h4>Welcome!</h4>    
+    <h4>Welcome!</h4>
+    <Link to='/game' type="button">
+      Game
+    </Link>
+
   </div>
 )
 
