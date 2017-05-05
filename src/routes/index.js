dc04bde3dc141938c07b719bd493435b7d626187
LeiJun <leijun87@hotmail.com>
2017-05-05 11:42:30 +0800

    add game route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,15 @@
 import CoreLayout from '../layouts/CoreLayout'
 import Home from './Home'
 import CounterRoute from './Counter'
+import GameRoute from './Game'
 
 export const createRoutes = (store) => ({
   path        : '/',
   component   : CoreLayout,
   indexRoute  : Home,
   childRoutes : [
-    CounterRoute(store)
+    CounterRoute(store),
+    GameRoute(store)
   ]
 })
 
